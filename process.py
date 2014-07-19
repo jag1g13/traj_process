ef90951871e95eeaa4da093a85d7240dbb014ebe
James Graham <J.Graham@software.ac.uk>
2014-07-19 02:41:59 +0100

    Optimised file read - only stores sugar atoms; optimised distance calculation - lower triangular numpy

--- process.py
+++ process.py
@@ -1,13 +1,15 @@
-import numpy
+#from __future__ import print_function
+import sys
+import numpy as np
 import re
 import time
-import cPickle
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
 
 sugar_atom_nums = {}
-sugar_atoms = ["C4", "O4", "HO4", "C3", "O3", "HO3", "C2", "O2", "HO2", "C6",\
-            "O6", "HO6", "C5", "O5", "C1", "O1", "HO1"]
+sugar_atoms = ["C4", "O4", "HO4", "C3", "O3", "HO3", "C2", "O2",\
+               "HO2", "C6", "O6", "HO6", "C5", "O5", "C1", "O1",\
+               "HO1"]
 
 class Atom:
     def __init__(self, atom_type, loc):
@@ -25,9 +27,8 @@ class Frame:
         """
         return Euclidean distance between two atoms
         """
-        #print("Distance between atoms {0} and {1}".format(num1, num2))
         dist = distance.euclidean(self.atoms[num1].loc, self.atoms[num2].loc)
-        #print(dist)
+        #print("Distance between atoms {0} and {1} is {2}".format(num1, num2, dist))
         return dist
 
     def show_atoms(self, start=0, end=-1):
@@ -35,23 +36,44 @@ class Frame:
         print coordinates of the atoms numbered start to end
         """
         print(self.title)
+        if end == -1:
+            end = len(self.atoms)
         for i in xrange(start, end):
             print(self.atoms[i].atom_type, self.atoms[i].loc)
 
-	
+    def get_all_dist(self):
+        size = len(sugar_atoms)
+        dists = np.zeros([size, size])
+        for i, atom1 in enumerate(sugar_atoms):
+            for j, atom2 in enumerate(sugar_atoms):
+                dists[i][j] = self.atom_dist(sugar_atom_nums[atom1], sugar_atom_nums[atom2])
+        return dists
+        
+    def get_all_dist_tri(self):
+        size = len(sugar_atoms)
+        dists = np.zeros([size, size])
+        for i in xrange(size):
+            for j in xrange(i):
+                dists[i][j] = self.atom_dist(sugar_atom_nums[sugar_atoms[i]], sugar_atom_nums[sugar_atoms[j]])
+            #print(dists[i])
+        dists = dists + dists.T - np.diag(dists.diagonal())
+        return dists
 
 def read(name, frame_max=float("inf")):
     """
     read frames from pdb file.  can set number of frames to read, default - all
     """
     global sugar_atom_nums
-    f = open(name, "r")
-    if not f:
-        print("Error opening file")
+    t_start = time.clock()
+    try:
+        f = open(name, "r")
+    except IOError:
+        print("IOError when trying to open file - is the filename correct?")
+        sys.exit(1)
     curr_frame = 0
     frames = [Frame(0)]
     coord_re2 = re.compile(r"\d{1,2}\.\d{3}")               #read all floats in the right format
-    type_re = re.compile(r"[A-Z]{1,2}\d")
+    #type_re = re.compile(r"[A-Z]{1,2}\d")
     lines = f.readlines()
     print("Reading file - length {0} lines".format(len(lines)))
     line_num = 0
@@ -59,7 +81,8 @@ def read(name, frame_max=float("inf")):
         line_num += 1
         perc = line_num * 100. / len(lines)
         if(line_num%100000 == 0):                           #print progress every 100k lines
-            print("{0}% done".format(perc))
+            sys.stdout.write("\r{:2.0f}% ".format(perc) + "X" * int(0.2*perc) + "-" * int(0.2*(100-perc)) )
+            sys.stdout.flush()
         line = line.strip("\n")
         #print(line)
         if line == "ENDMDL":                                #end of frame
@@ -84,56 +107,37 @@ def read(name, frame_max=float("inf")):
                 print("Frame number incorrect")
             continue
         coords = [float(num) for num in re.findall(coord_re2, line)]
-        #print(line)
-        #atom_type = re.search(type_re, line).group()
         atom_type = line[12:17].strip()
-        #print(coords)
-        #frames[curr_frame].atoms.append(numpy.array(coords))    #must be a normal line, read coords
-        frames[curr_frame].atoms.append(Atom(atom_type, numpy.array(coords)))    #must be a normal line, read coords
-        if curr_frame == 1:
-            sugar_atom_nums[atom_type] = int(line[7:11].strip())
+        if atom_type in sugar_atoms:
+            frames[curr_frame].atoms.append(Atom(atom_type, np.array(coords)))    #must be a normal line, read coords
+            if curr_frame == 1:
+                sugar_atom_nums[atom_type] = int(line[7:11].strip()) - 1            #which atom number is each of the sugar atoms
     frames.pop(-1)                                              #get rid of last frame, it's empty
-    del(sugar_atom_nums["HW1"])                                 #remove entries for water from dictionary, don't need them
-    del(sugar_atom_nums["HW2"])
-    del(sugar_atom_nums["OW"])
-    print("Read {0} frames".format(len(frames)))
-    print(sugar_atom_nums)
+    t_end = time.clock()
+    print("\rSuccessfully read {0} frames in {1}s".format(len(frames), (t_end - t_start)))
+    #print(sugar_atom_nums)
     return frames
 
-def get_all_dist(frame):
+def calc_dists(frames):
+    t_start = time.clock()
     dists = []
-    for atom1 in sugar_atoms:
-        dists.append([])
-        for atom2 in sugar_atoms:
-            dists[-1].append(frame.atom_dist(sugar_atom_nums[atom1], sugar_atom_nums[atom2]))
-        #print(dists[-1])
+    for i, frame in enumerate(frames):
+        perc = i * 100. / len(frames)
+        #print("Frame {0}".format(frame.num))
+        if(i%100 == 0):
+            sys.stdout.write("\r{:2.0f}% ".format(perc) + "X" * int(0.2*perc) + "-" * int(0.2*(100-perc)) )
+            sys.stdout.flush()
+        dists.append(frame.get_all_dist_tri())
+    t_end = time.clock()
+    print("\rSuccessfully calculated {0} frames in {1}s".format(len(frames), (t_end - t_start)))
     return dists
 
+
 if __name__ == "__main__":
-    start_frame = time.clock()
     frames = read(r"test_cases/md.pdb")
-    #frames = cPickle.load(open("frames.pickle", "rb"))
-    end_frame =  time.clock()
-    print(end_frame - start_frame)
-    #start_pickle = time.clock()
-    #cPickle.dump(frames, open("frames.pickle", "wb"))
-    #end_pickle = time.clock()
-    #print(end_pickle - start_pickle)
-    #print(frames[1].atoms[0:20])
-    frames[0].show_atoms(0,20)
-    #print(frames[-1].atoms[0:20])
-    frames[-1].show_atoms(0,20)
-    #frames[0].atom_dist(0,1)
-    dist = []
-    start_dist = time.clock()
-    for frame in frames:
-        #dist.append(frame.atom_dist(0,1))
-        #print("Frame {0}".format(frame.num))
-        dists = get_all_dist(frame)
-    for line in dists:
-        print(line)
-    end_dist = time.clock()
-    #print(numpy.average(dist))
+    dists = calc_dists(frames)
+    avg = np.mean(dists, axis=0)
     #plt.hist(dist, 100)
     #plt.show()
-    print(end_dist - start_dist)
+    np.set_printoptions(precision=3, suppress=True, linewidth=len(sugar_atoms)*7+4)
+    print(avg)
